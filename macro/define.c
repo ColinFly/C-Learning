9be7c8d7a5d9f144a89446f724e054e00be1e421
colin <29867547@qq.com>
2016-03-19 17:03:20 +0800

    预处理的理解,宏,typedef的使用

---
+++ macro/define.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+# define R 20;
+#define M int main(
+# define ADD(a,b) (a+b)
+
+typedef int tni;
+M)
+{
+  tni a=R;
+  printf("a=%d\n",a);
+  int b=ADD(5,5);
+  printf("b=%d\n",b);
+
+}
